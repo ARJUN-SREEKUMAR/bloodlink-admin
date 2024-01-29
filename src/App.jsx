42ce79758acf7d82e208af096c41f73fc4873c7d
Arjun S <72598741+ARJUN-SREEKUMAR@users.noreply.github.com>
2024-01-29 19:00:09 +0530

    inital commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <h1 className=" p-16 text-3xl font-bold underline text-center">
+      Hello world!
+    </h1>
+    </>
+  )
+}
+
+export default App
