91ed29cb09e23c399df3ce5cdc230579656634bf
Arjun S <72598741+ARJUN-SREEKUMAR@users.noreply.github.com>
2024-01-29 19:04:48 +0530

    ini commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <>
-      <h1 className=" p-16 text-3xl font-bold underline text-center">
+      <h1 className=" p-24 text-3xl font-bold underline text-center">
       Hello world!
     </h1>
     </>
